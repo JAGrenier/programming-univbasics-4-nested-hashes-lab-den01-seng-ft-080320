ff1de4c5fbf57cb2982352934aad30025820c3b6
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-16 16:46:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,7 +18,7 @@ def hopper
 
 
 grace_hopper = programmer_hash[:grace_hopper]
-end  
+end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
